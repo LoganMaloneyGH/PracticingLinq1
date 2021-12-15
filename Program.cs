080373b122ef1c3a68b7d61861ec301f3be88873
logan Maloney <shakenasty13@gmail.com>
2021-12-15 14:56:31 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGamesList = new List<string>()
+            {
+                "Halo",
+                "Rust",
+                "Doom Eternal",
+                "Pub G",
+                "GTA"
+            };
+
+            var gamesListOrdered = videoGamesList.OrderBy(name => name.Length).ToList();
+
+            //foreach (var gameName in gamesListOrdered)
+            //{
+            //    Console.WriteLine(gameName);
+            //}
+
+            gamesListOrdered.ForEach(game => Console.WriteLine(game));
+        }
+    }
+}
